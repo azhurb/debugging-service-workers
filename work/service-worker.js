8e79b2a961fd422e418f7da06f5c060d09a34199
alex <zhurbitsky@gmail.com>
2018-01-04 10:07:45 +0200

    Testing Push notifications

--- work/service-worker.js
+++ work/service-worker.js
@@ -47,3 +47,17 @@ self.addEventListener('fetch', function(event) {
             )
     );
 });
+
+self.addEventListener('push', function(event) {
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = '/images/smiley.svg';
+    var tag = 'simple-push-example-tag';
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+        })
+    );
+});
